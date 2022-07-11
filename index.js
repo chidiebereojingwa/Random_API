16d5fec301111c86946bde2602669f77a270af71
chidiebereojingwa <ojingwaoji@gmail.com>
2022-07-12 00:07:30 +0100

    third

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const fs = require('fs');
+const { resolve } = require('path');
 const superagent = require('superagent');
+
+const readFilePro = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if(err) reject('I could not find that file ');
+            resolve(data)
+        })
+    })
+}
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
