26935817ca8806090c47d4bdbd44f4df64589e1b
chidiebereojingwa <ojingwaoji@gmail.com>
2022-07-15 03:07:50 +0100

    made changes as reviewed

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+const fs = require('fs')
 const express = require('express');
 
-const app = express;
+const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from server side!');
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello from server side!', app: 'Node.s' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).send('you can post on this endpoint...');
+// });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours
+    }
+  });
+})
 
 const port = 3000;
 app.listen(port, () => {
