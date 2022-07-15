cc5d26b473f761b9f6a9905777489607b3793fe0
chidiebereojingwa <ojingwaoji@gmail.com>
2022-07-15 04:37:32 +0100

    add post request

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 
 const app = express();
 
+// middleware
+app.use(express.json())
+
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Hello from server side!', app: 'Node.s' });
 // });
@@ -20,6 +23,19 @@ app.get('/api/v1/tours', (req, res) => {
       tours
     }
   });
+})
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({id: newId}, req.body);
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+     res.status(201).json({
+      status: 'success',
+      data: newTour
+     })
+  });
+
 })
 
 const port = 3000;
